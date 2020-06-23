0fa191a1ae563fc54eff52e06058acece3291a1b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-24 00:56:42 +0200

    fix: use proper firestore collection for osowiec log

--- project.config.sh
+++ project.config.sh
@@ -3,7 +3,7 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GCP_PROJECT_ID="inspector-widget"
 export GCP_FUNCTIONS_REGION="europe-west3"
-export FIRESTORE_COLLECTION="/osowieclog"
+export FIRESTORE_COLLECTION="osowieclog"
 export API_SECRET_KEYS=""
 
 source "${PROJECT_DIR}/secret.config.sh"
