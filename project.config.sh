d20ac8b612cd88167f40f2f12ed9a802502ff23b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-23 22:31:34 +0200

    chore: suport secret config

--- project.config.sh
+++ project.config.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export GCP_PROJECT_ID=""
+export GCP_PROJECT_ID="inspector-widget"
 export GCP_FUNCTIONS_REGION="europe-west3"
-export FIRESTORE_COLLECTION="/log"
-export API_SECRET_KEYS="aaaaa"
+export FIRESTORE_COLLECTION="/"
+export API_SECRET_KEYS=""
+
+source "${PROJECT_DIR}/secret.config.sh"
